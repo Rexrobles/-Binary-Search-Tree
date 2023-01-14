842dab266b802655c38036e60bdf7ba536431cb0
Rexrobles <reximma03@gmail.com>
2023-01-14 13:53:36 +0800

    added print func() to check if pre_order and post_order func() is working

--- exer1_binary_search_tree.py
+++ exer1_binary_search_tree.py
@@ -101,3 +101,5 @@ if __name__ == '__main__':
     print("Min:",numbers_tree.find_min())
     print("Max:",numbers_tree.find_max())
     print("Sum:",numbers_tree.calculate_sum())
+    print("Post order traversal:", numbers_tree.post_order_traversal())
+    print("Pre order traversal:", numbers_tree.pre_order_traversal())
