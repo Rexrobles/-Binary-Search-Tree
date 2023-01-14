0dcfc23c0f2939fb8786e1c1320ea08c1a3a63a2
Rexrobles <reximma03@gmail.com>
2023-01-14 17:32:38 +0800

    Added add_child function.

--- my_name_binary_search_tree.py
+++ my_name_binary_search_tree.py
@@ -5,3 +5,21 @@ class BinarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+    
+    def add_child (self, data):
+        if data == self.data:
+            return  # if the node already exist
+
+        # If data is less than the value of current node --goes to left subtree
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data) 
+            else:
+                self.left = BinarySearchTreeNode(data)
+
+        # If data is greater than the value of current node --goes to right subtree
+        else:
+            if self.right:
+                self.right.add_child(data) 
+            else:
+                self.right = BinarySearchTreeNode(data)
