4e56f619625dfb787431e867c5c84a51b4ed5da8
Rexrobles <reximma03@gmail.com>
2023-01-14 11:11:35 +0800

    add condition If data is less than the value of current node it will go to left subtree

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -4,6 +4,15 @@ class BinarySearchTreeNode:
         self.left = None
         self.right = None
         
-def add_child(self, data):
+    def add_child(self, data):
         if data == self.data:
             return # if the node already exist
+        
+        # If data is less than the value of current node --goes to left subtree
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        
+        
