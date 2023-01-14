088569a4438241297fbe3156c8b9e11305affc01
Rexrobles <reximma03@gmail.com>
2023-01-14 13:07:40 +0800

    added print function to check if the find_max func() is working

--- exer1_binary_search_tree.py
+++ exer1_binary_search_tree.py
@@ -77,3 +77,4 @@ if __name__ == '__main__':
     numbers = [17, 4, 1, 20, 9, 23, 18, 34]
     numbers_tree = build_tree(numbers)
     print("Min:",numbers_tree.find_min())
+    print("Max:",numbers_tree.find_max())
